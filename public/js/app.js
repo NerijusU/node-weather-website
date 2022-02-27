2d5e6a9f0a8f22944484523fe37fd424bc945db0
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-02-27 19:44:25 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client side js file')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location )
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                const {temperature, feelslike, description} = data.forecast
+                messageOne.textContent = data.location
+                console.log(data.forecast)
+                messageTwo.textContent = `temperature: ${temperature}; feelslike: ${feelslike}; description: ${description}`
+            }
+        })
+})
